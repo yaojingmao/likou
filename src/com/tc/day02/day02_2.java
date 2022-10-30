2c876dd6f926034c85686ebef79605ec9e6c863c
ittang <1324606828@.qq.com>
2022-10-30 17:11:30 +0800

    第五题提交

--- src/com/tc/day02/day02_2.java
+++ src/com/tc/day02/day02_2.java
@@ -1,6 +1,6 @@
 package com.tc.day02;
 
-
+//第四题
 public class day02_2 {
     public static void main(String[] args) {
         int[] nums1 = new int[]{1, 3};
@@ -13,6 +13,7 @@ public class day02_2 {
         int l1 = nums1.length;
         int l2 = nums2.length;
         int[] nums3 = new int[l1 + l2];
+//        如果一个数组为0那么中位数就是另一个数组的中位数了
         if (l1 == 0) {
             if (l2 % 2 == 0) {
                 return (nums2[l2 / 2 - 1] + nums2[l2 / 2]) / 2.0;
@@ -27,25 +28,27 @@ public class day02_2 {
                 return nums1[l1 / 2];
             }
         }
+
         int i = 0;
         int j = 0;
         int count = 0;
+//        将两个数组合到一个数组中
         while (count != (l1 + l2)) {
-
+//nums1[]数组已经没值了则将nums2[]数组全都添加到nums3[]数组中
             if (i == l1) {
                 while (j != l2) {
                     nums3[count++] = nums2[j++];
                 }
                 break;
             }
-
+//nums2[]数组已经没值了则将nums1[]数组全都添加到nums3[]数组中
             if (j == l2) {
                 while (i != l1) {
                     nums3[count++] = nums1[i++];
                 }
                 break;
             }
-
+//将值小的先放入nums3数组中
             if (nums1[i] < nums2[j]) {
                 nums3[count++] = nums1[i++];
             } else {
--- src/com/tc/day03/day03.java
+++ src/com/tc/day03/day03.java
@@ -1,4 +1,41 @@
 package com.tc.day03;
 
+//第五题
 public class day03 {
+
+
+    public String longestPalindrome(String s) {
+
+        if (s.length()<2){
+            return s;
+        }
+
+        int length = s.length();
+        int begin = 0;
+        int maxLen = 1;
+        char[] chars = s.toCharArray();
+//        如果i等于4了j就不满足条件了
+        for (int i = 0; i < length - 1; i++) {
+//            chars数组最大下标就是length-1
+            for (int j = i + 1; j < length; j++) {
+                if (j - i + 1 > maxLen && isbumber(chars, i, j)) {
+                    maxLen = j - i + 1;
+                    begin = i;
+                }
+            }
+        }
+        return s.substring(begin, begin + maxLen);
+    }
+
+    //回文数就是左边等于右边
+    private boolean isbumber(char[] chars, int left, int right) {
+        while (left < right) {
+            if (chars[left] != chars[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
 }
